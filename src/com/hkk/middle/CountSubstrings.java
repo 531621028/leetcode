3345b9930846f575bff665d5b6a726e81f83026e
hukangkang <hukangkang@xiaomi.com>
2023-03-27 19:31:07 +0800

    粉刷匠每日一题

---
+++ src/com/hkk/middle/CountSubstrings.java
@@ -0,0 +1,40 @@
+package com.hkk.middle;
+
+/**
+ * @author kang
+ * @date 2023/3/27
+ */
+public class CountSubstrings {
+
+
+    public int countSubstrings(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dpl = new int[m + 1][n + 1];
+        int[][] dpr = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dpl[i + 1][j + 1] = s.charAt(i) == t.charAt(j) ? (dpl[i][j] + 1) : 0;
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                dpr[i][j] = s.charAt(i) == t.charAt(j) ? (dpr[i + 1][j + 1] + 1) : 0;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s.charAt(i) != t.charAt(j)) {
+                    ans += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new CountSubstrings().countSubstrings("aba", "baba"));
+    }
+
+}
