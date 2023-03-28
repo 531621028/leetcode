5c1ca143363703c85a0196cef007c75b26426894
hukangkang <hukangkang@xiaomi.com>
2023-03-28 20:33:37 +0800

    粉刷匠每日一题

---
+++ src/com/hkk/hard/ShortestCommonSuperSequence.java
@@ -0,0 +1,29 @@
+package com.hkk.hard;
+
+/**
+ * @author kang
+ * @date 2023/3/28
+ */
+public class ShortestCommonSuperSequence {
+
+    public String shortestCommonSuperSequence(String str1, String str2) {
+
+        if(str1.length() > str2.length()){
+
+        }
+
+    }
+
+    public String shortestSuperSequence(String str1, String str2) {
+
+
+        if(str1.length() > str2.length()){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ShortestCommonSuperSequence().shortestCommonSuperSequence("", ""));
+    }
+}
